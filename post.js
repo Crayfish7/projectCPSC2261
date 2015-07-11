83b8738f391fc730ca07b9412453883806c8fefc
Crayfish7 <lesleynmak@hotmail.com>
2015-07-11 14:23:06 -0700

    javascript for the post.html

---
+++ post.js
@@ -0,0 +1,25 @@
+    var myApp = angular.module('myApp', []);
+    myApp.controller('postController', function($scope, $http) {
+      var dataobj = {};
+      
+      $scope.postStory = function() {
+        dataObj = {
+          profile_id : $scope.profile_id,
+          name : $scope.userName,
+          email : $scope.email,
+          title : $scope.storyTitle,
+          story : $scope.story,
+          url : $scope.url
+        };
+        $http.post('http://jsonstub.com/message/post', dataObj).
+          success(function(data, status, headers, config) {
+            // this callback will be called asynchronously
+            // when the response is available
+            alert(JSON.stringify(data));
+        }).
+        error(function(data, status, headers, config) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+        });
+      }
+    });
