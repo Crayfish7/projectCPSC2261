4628a9682ad13d70129777cdcf0786cc66b3c928
ycyney <ycyney@users.noreply.github.com>
2015-07-18 18:47:54 -0800

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ var db = mongojs('test',["messages"]);
 app = express();
 app.use(bodyParser.json());
 
+app.all('*', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+ });
+
 app.get("/messages/all", function (req, res) {	
 	db.messages.find(function(err, records) {
 		if (records.length == 0) {				
@@ -26,6 +33,7 @@ app.get("/messages/main", function (req, res){
 	});
 
 app.put("/messages/put", function (req,res){
+	
 	var profile = req.body;
 	db.messages.insert(profile, function(err, records) {
 		if(err) {
@@ -49,7 +57,7 @@ app.post("/messages/weirdness", function (req,res){
 	});
 });
 
-var server = app.listen(1337, function () {
+var server = app.listen(1338, function () {
 
     var host = server.address().address;
     var port = server.address().port;
